9d9385d39979955b8dda06af6811543e7d74feac
Belinda-BTM <tanabelmand@gmail.com>
2026-05-30 11:30:34 +0200

    Lab1: Added ConsoleClient with ServiceReference, successfully calls SayHello

---
+++ CurrencyExchangeOffice/Lab1.ConsoleClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Lab1.ConsoleClient.ServiceReference1;
+
+namespace Lab1.ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Service1Client client = new Service1Client();
+
+            string result = client.SayHello("Belinda");
+            Console.WriteLine(result);
+
+            client.Close();
+            Console.ReadLine();
+        }
+    }
+}
