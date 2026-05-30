604c1179b6a0120f9f276fe8bad6063e524e90e3
Belinda-BTM <tanabelmand@gmail.com>
2026-05-30 13:39:37 +0200

    Lab2: Defined currency service contract and NBP API implementation

---
+++ CurrencyExchangeOffice/Lab2.CurrencyService/IService1.cs
@@ -0,0 +1,11 @@
+﻿using System.ServiceModel;
+
+namespace Lab2.CurrencyService
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string GetExchangeRate(string currencyCode);
+    }
+}
---
+++ CurrencyExchangeOffice/Lab2.CurrencyService/Service1.svc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+
+namespace Lab2.CurrencyService
+{
+    public class Service1 : IService1
+    {
+        public string GetExchangeRate(string currencyCode)
+        {
+            try
+            {
+                string url = $"http://api.nbp.pl/api/exchangerates/rates/A/{currencyCode}/?format=json";
+
+                using (WebClient client = new WebClient())
+                {
+                    string response = client.DownloadString(url);
+                    return response;
+                }
+            }
+            catch (Exception ex)
+            {
+                return "Error: " + ex.Message;
+            }
+        }
+    }
+}
