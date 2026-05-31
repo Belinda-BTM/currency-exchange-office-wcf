77a96cf9737a65952bb42569deccb2e0debb2db3
Belinda-BTM <tanabelmand@gmail.com>
2026-05-31 11:26:42 +0200

    Lab6: Implemented CalculateExchange method with NBP rate conversion logic

--- CurrencyExchangeOffice/ExchangeOffice/Service1.svc.cs
+++ CurrencyExchangeOffice/ExchangeOffice/Service1.svc.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Net;
+using System.Text;
+using System.Web.Script.Serialization;
 
 namespace ExchangeOffice
 {
     public class Service1 : IService1
     {
+        private decimal GetRateFromNBP(string currencyCode)
+        {
+            if (currencyCode.ToUpper() == "PLN")
+                return 1.0m;
+
+            string url = $"http://api.nbp.pl/api/exchangerates/rates/A/{currencyCode}/?format=json";
+
+            using (WebClient client = new WebClient())
+            {
+                client.Encoding = Encoding.UTF8;
+                string json = client.DownloadString(url);
+                var serializer = new JavaScriptSerializer();
+                dynamic data = serializer.DeserializeObject(json);
+                return data["rates"][0]["mid"];
+            }
+        }
+
         public decimal CalculateExchange(string fromCurrency, string toCurrency, decimal amount)
         {
-            throw new NotImplementedException("Will be implemented in Lab 6");
+            try
+            {
+                decimal fromRate = GetRateFromNBP(fromCurrency.ToUpper());
+                decimal toRate = GetRateFromNBP(toCurrency.ToUpper());
+
+                decimal amountInPLN = amount * fromRate;
+                decimal result = amountInPLN / toRate;
+
+                return Math.Round(result, 2);
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
         }
 
         public string GetExchangeRate(string currencyCode)
