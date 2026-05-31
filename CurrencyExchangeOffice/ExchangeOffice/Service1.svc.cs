6bd441569180b0e5a04863dd528eb7ab168d3280
Belinda-BTM <tanabelmand@gmail.com>
2026-05-31 11:39:37 +0200

    Lab7: Integrated NBP API - implemented GetExchangeRate and GetAvailableCurrencies

--- CurrencyExchangeOffice/ExchangeOffice/Service1.svc.cs
+++ CurrencyExchangeOffice/ExchangeOffice/Service1.svc.cs
@@ -44,12 +44,65 @@ namespace ExchangeOffice
 
         public string GetExchangeRate(string currencyCode)
         {
-            throw new NotImplementedException("Will be implemented in Lab 7");
+            try
+            {
+                string url = $"http://api.nbp.pl/api/exchangerates/rates/A/{currencyCode}/?format=json";
+
+                using (WebClient client = new WebClient())
+                {
+                    client.Encoding = Encoding.UTF8;
+                    string json = client.DownloadString(url);
+                    var serializer = new JavaScriptSerializer();
+                    dynamic data = serializer.DeserializeObject(json);
+
+                    string currency = data["currency"];
+                    string code = data["code"];
+                    decimal rate = data["rates"][0]["mid"];
+                    string date = data["rates"][0]["effectiveDate"];
+
+                    return $"Currency: {currency} ({code})\nRate: {rate} PLN\nDate: {date}";
+                }
+            }
+            catch (WebException)
+            {
+                return $"Error: '{currencyCode}' is not a valid currency code.";
+            }
+            catch (Exception ex)
+            {
+                return "Error: " + ex.Message;
+            }
         }
 
         public string GetAvailableCurrencies()
         {
-            throw new NotImplementedException("Will be implemented in Lab 7");
+            try
+            {
+                string url = "http://api.nbp.pl/api/exchangerates/tables/A/?format=json";
+
+                using (WebClient client = new WebClient())
+                {
+                    client.Encoding = Encoding.UTF8;
+                    string json = client.DownloadString(url);
+                    var serializer = new JavaScriptSerializer();
+                    dynamic data = serializer.DeserializeObject(json);
+
+                    StringBuilder result = new StringBuilder();
+                    result.AppendLine("Available currencies:");
+                    result.AppendLine("====================");
+
+                    var rates = data[0]["rates"];
+                    foreach (var rate in rates)
+                    {
+                        result.AppendLine($"{rate["code"]} - {rate["currency"]}");
+                    }
+
+                    return result.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                return "Error: " + ex.Message;
+            }
         }
     }
 }
