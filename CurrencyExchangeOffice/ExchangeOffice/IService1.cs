6e3ac571226969648bc9fd34f5cf747a055291e4
Belinda-BTM <tanabelmand@gmail.com>
2026-05-31 11:22:33 +0200

    Lab5: ExchangeOffice project created, service architecture and contracts defined

---
+++ CurrencyExchangeOffice/ExchangeOffice/IService1.cs
@@ -0,0 +1,17 @@
+﻿using System.ServiceModel;
+
+namespace ExchangeOffice
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        decimal CalculateExchange(string fromCurrency, string toCurrency, decimal amount);
+
+        [OperationContract]
+        string GetExchangeRate(string currencyCode);
+
+        [OperationContract]
+        string GetAvailableCurrencies();
+    }
+}
---
+++ CurrencyExchangeOffice/ExchangeOffice/Service1.svc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExchangeOffice
+{
+    public class Service1 : IService1
+    {
+        public decimal CalculateExchange(string fromCurrency, string toCurrency, decimal amount)
+        {
+            throw new NotImplementedException("Will be implemented in Lab 6");
+        }
+
+        public string GetExchangeRate(string currencyCode)
+        {
+            throw new NotImplementedException("Will be implemented in Lab 7");
+        }
+
+        public string GetAvailableCurrencies()
+        {
+            throw new NotImplementedException("Will be implemented in Lab 7");
+        }
+    }
+}
